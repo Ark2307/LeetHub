600cdff777d52b5e093cd1d7347491b6cb60efbf
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-03-31 09:17:19 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.18%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0 , b = 1 , c = a + b ;
+        if(n == 0)
+            return a ;
+        
+        if(n == 1)
+            return b ;
+        
+        for(int i = 2 ; i <= n ; i++){
+            c = a + b ;
+            a = b ;
+            b = c ;
+        }
+        return c ;
+    }
+};
