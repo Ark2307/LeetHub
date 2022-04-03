75d30045e6ce362a2e8da5fbe63bcdd954f8173f
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-04-03 17:27:14 +0530

    Time: 291 ms (38.95%), Space: 8.3 MB (65.45%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string longestPalindrome(string str) {
         
+        //Logical Approach
+        
         int n = str.size() ;
         int maxi = 0 , start = 0 ;
         
@@ -25,9 +27,47 @@ public:
             }
         }
         
-        string res = str.substr(start , maxi) ;
+        string res1 = str.substr(start , maxi) ;
         
-        return res ;
+        
+        // Now I will try with the DP approach
+        bool dp[n][n] ;
+        memset(dp , 0 , sizeof(dp)) ;
+        
+        //dp[i][j] denotes if substr from i to j is palindrome
+        
+        for(int i = 0 ; i < n ; i++){
+            dp[i][i] = 1 ;
+        }
+        
+        int startIn = 0 , maxi1 = 1 ;
+        
+        for(int i = 0 ; i < n - 1; i++){
+            if(str[i] == str[i + 1])
+            {
+                startIn = i ;
+                dp[i][i + 1] = 1 ;
+                maxi1 = 2 ;
+            }
+        }
+        
+        for(int len = 3 ; len <= n ; len++){
+            for(int start = 0 ; start <= (n - len) ; start++){
+                int end = start + len - 1 ;
+                
+                if(str[start] == str[end] && dp[start + 1][end - 1]){
+                    dp[start][end] = 1 ;
+                    if(len > maxi1){
+                        startIn = start ;
+                        maxi1 = len ;
+                    }
+                }
+            }
+        }
+        
+        string res2 = str.substr(startIn , maxi1) ;
+        
+        return res2 ;
         
     }
 };
