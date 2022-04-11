af8587545e4b2d1e5abc64d8eda54611120eb01c
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-04-11 09:37:04 +0530

    Time: 36 ms (21.75%), Space: 9.9 MB (99.29%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -4,40 +4,59 @@ public:
         int n = nums.size() ;
         sort(nums.begin() , nums.end()) ;
         
-        vector<int>::iterator itr , itr1;
-        int res = INT_MAX ,ans;
         
-        for(int i = 0 ; i < n - 2 ; i++){
-            for(int j = i + 1 ; j < n - 1 ; j++){
-                int val = target - nums[i] - nums[j] ;
-                itr = nums.begin() ;
-                advance(itr , j) ;
-                itr++ ;
+        // My Accepted approach .. TC - N2logn
+        
+//         vector<int>::iterator itr , itr1;
+//         int res = INT_MAX ,ans;
+        
+//         for(int i = 0 ; i < n - 2 ; i++){
+//             for(int j = i + 1 ; j < n - 1 ; j++){
+//                 int val = target - nums[i] - nums[j] ;
+//                 itr = nums.begin() ;
+//                 advance(itr , j) ;
+//                 itr++ ;
                 
-                itr1 = upper_bound(itr , nums.end() , val) ;
-                //cout << itr1 - nums.begin() << " " << val << " " ;
-                if(itr1 != nums.end())
-                {
-                    int x = *itr1 ;
-                    int diff = abs(target - x - nums[i] - nums[j]) ;
-                    if(diff < res){
-                        res = diff ;
-                        ans = x + nums[i] + nums[j] ;
-                    }
-                }
+//                 itr1 = upper_bound(itr , nums.end() , val) ;
+//                 //cout << itr1 - nums.begin() << " " << val << " " ;
+//                 if(itr1 != nums.end())
+//                 {
+//                     int x = *itr1 ;
+//                     int diff = abs(target - x - nums[i] - nums[j]) ;
+//                     if(diff < res){
+//                         res = diff ;
+//                         ans = x + nums[i] + nums[j] ;
+//                     }
+//                 }
                 
-                if(itr1 != itr)
-                    itr1-- ;
+//                 if(itr1 != itr)
+//                     itr1-- ;
                 
-                int x = *itr1 ;
-                //cout << x << " " ;
-                int diff = abs(target - x - nums[i] - nums[j]) ;
-                if(diff < res){
-                    res = diff ;
-                    ans = x + nums[i] + nums[j] ;
-                }
+//                 int x = *itr1 ;
+//                 //cout << x << " " ;
+//                 int diff = abs(target - x - nums[i] - nums[j]) ;
+//                 if(diff < res){
+//                     res = diff ;
+//                     ans = x + nums[i] + nums[j] ;
+//                 }
+//             }
+//         }
+//         return ans ;
+        
+        // TC-> n2 {Two pointer Approach}
+        
+        int best = INT_MAX;
+        for (int i = 0; i < n - 2; ++i) {
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int gap = nums[i] + nums[l] + nums[r] - target;
+                if (abs(gap) < abs(best)) best = gap;
+                else if (gap < 0)
+                    ++l;
+                else
+                    --r;
             }
         }
-        return ans ;
+        return target + best;
     }
 };
