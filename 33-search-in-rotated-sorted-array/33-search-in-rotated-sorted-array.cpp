2030923de27a338b07946e3002e3d7c7f28bf5c5
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-04-13 11:43:51 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (29.49%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -3,53 +3,82 @@ public:
     int search(vector<int>& nums, int target) {
         int n = nums.size() ;
         // first i will try to find the pivot element.
-        int lo = 0 , hi = n - 1 , mid , pivInd = -1;
+        
+//         int lo = 0 , hi = n - 1 , mid , pivInd = -1;
+//         while(lo <= hi){
+//             mid = (lo + hi) / 2 ;
+//             if(mid < hi && nums[mid] > nums[mid + 1]){
+//                 pivInd = mid ;
+//                 break ;
+//             }
+            
+//             if(mid > lo && nums[mid] < nums[mid - 1]){
+//                 pivInd = mid - 1; 
+//                 break ;
+//             }
+               
+//             if(nums[mid] > nums[lo])
+//                lo = mid + 1 ;
+               
+//             else
+//                hi = mid - 1 ;
+//         }
+        
+//         //cout << pivInd << " " ;
+        
+//         hi = n - 1 , lo = pivInd + 1 ;
+//         while(lo <= hi){
+//             mid = (lo + hi) / 2; 
+//             if(nums[mid] == target)
+//                 return mid ;
+            
+//             if(nums[mid] > target)
+//                 hi = mid - 1 ;
+            
+//             else
+//                 lo = mid + 1 ;
+//         }
+               
+//         hi = pivInd , lo = 0 ;
+//         while(lo <= hi){
+//             mid = (lo + hi) / 2; 
+//             if(nums[mid] == target)
+//                 return mid ;
+            
+//             if(nums[mid] > target)
+//                 hi = mid - 1 ;
+            
+//             else
+//                 lo = mid + 1 ;
+//         }
+//         return -1 ;
+        
+        
+        // now without finding pivot element
+        
+        int lo = 0 , hi = n - 1 , mid ;
         while(lo <= hi){
             mid = (lo + hi) / 2 ;
-            if(mid < hi && nums[mid] > nums[mid + 1]){
-                pivInd = mid ;
-                break ;
+            if(nums[mid] == target){
+                return mid ;
             }
             
-            if(mid > lo && nums[mid] < nums[mid - 1]){
-                pivInd = mid - 1; 
-                break ;
+            if(nums[lo] <= nums[mid]){
+                if(target >= nums[lo] && target <= nums[mid])
+                    hi = mid - 1 ;
+                
+                else
+                    lo = mid + 1 ;
             }
-               
-            if(nums[mid] > nums[lo])
-               lo = mid + 1 ;
-               
+            
+            else if(target >= nums[mid] && target <= nums[hi]){
+                lo = mid + 1 ;
+            }
+            
             else
-               hi = mid - 1 ;
+                hi = mid - 1 ;
         }
         
-        //cout << pivInd << " " ;
-        
-        hi = n - 1 , lo = pivInd + 1 ;
-        while(lo <= hi){
-            mid = (lo + hi) / 2; 
-            if(nums[mid] == target)
-                return mid ;
-            
-            if(nums[mid] > target)
-                hi = mid - 1 ;
-            
-            else
-                lo = mid + 1 ;
-        }
-               
-        hi = pivInd , lo = 0 ;
-        while(lo <= hi){
-            mid = (lo + hi) / 2; 
-            if(nums[mid] == target)
-                return mid ;
-            
-            if(nums[mid] > target)
-                hi = mid - 1 ;
-            
-            else
-                lo = mid + 1 ;
-        }
         return -1 ;
     }
 };
