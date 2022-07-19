e11179d7664381b7c21a47968fdddc1136ee6768
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-07-19 16:57:52 +0530

    Time: 74 ms (99.30%), Space: 69.5 MB (14.11%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size() ;
-        int TotalCost = 0 , TotalFuel = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            TotalCost += cost[i] ;
-            TotalFuel += gas[i] ;
-        }
+        // int n = gas.size() ;
+//         int TotalCost = 0 , TotalFuel = 0 ;
+//         for(int i = 0 ; i < n ; i++){
+//             TotalCost += cost[i] ;
+//             TotalFuel += gas[i] ;
+//         }
         
-        if(TotalCost > TotalFuel)
-            return -1 ;
+//         if(TotalCost > TotalFuel)
+//             return -1 ;
         
-        int ind , curr = -1 ;
-        for(int i = 0 ; i < n ; i++){
-            if(curr < 0){
-                curr = 0 ;
-                ind = i ;
-            }
+//         int ind , curr = -1 ;
+//         for(int i = 0 ; i < n ; i++){
+//             if(curr < 0){
+//                 curr = 0 ;
+//                 ind = i ;
+//             }
             
-            curr += (gas[i] - cost[i]) ;
+//             curr += (gas[i] - cost[i]) ;
+//         }
+        
+//         return ind ;
+        
+        int count = 0 , neg = 0;
+        int ind = 0 ;
+        for(int i = 0 ; i < gas.size() ; i++){
+            count += (gas[i] - cost[i]) ;
+            if(count < 0){
+                neg += count ;
+                count = 0 ;
+                ind = i + 1 ;
+            }
         }
+        
+        if((count + neg) < 0)
+            return -1 ;
         
         return ind ;
     }
