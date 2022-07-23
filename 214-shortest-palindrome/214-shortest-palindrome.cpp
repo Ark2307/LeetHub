e2f10cb3d5f02d6c11ec13a7569b2c2994eb0a7a
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-07-23 18:48:47 +0530

    Time: 5 ms (59.89%), Space: 7.2 MB (91.04%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.cpp
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int m = s.size() , n = (2 * m) + 1 ;
-        
-        string rev = s ;
-        reverse(rev.begin() , rev.end()) ;
-        
-        s.push_back('#') ;
-        s += rev ;
-        
-        int lps[n] ;
-        lps[0] = 0 ;
-        int i = 1 , curr = 0 ;
-        
-        while(i < n){
-            if(s[i] == s[curr]){
-                curr++ ;
-                lps[i] = curr ;
-                i++ ;
-            }
-            
-            else{
-                if(curr != 0){
-                    curr = lps[curr - 1] ;
-                }
-                
-                else{
-                    lps[i] = 0 ;
-                    i++ ;
-                }
-            }
-                 
+        int n = s.size();
+        int i = 0;
+        for (int j = n - 1; j >= 0; j--) {
+            if (s[i] == s[j])
+                i++;
         }
-        
-        int add = m - lps[n - 1] ;
-        // cout << add << " " ;
-        string str = rev.substr(0 , add) ;
-        reverse(str.begin() , str.end()) ;
-        rev += str ;
-        
-        return rev ;
+        if (i == n) {
+            return s;
+        }
+        string remain_rev = s.substr(i, n);
+        reverse(remain_rev.begin(), remain_rev.end());
+        return remain_rev + shortestPalindrome(s.substr(0, i)) + s.substr(i);
     }
 };
