ec71f6176596c8b0eb864ecfe22203a7a83e145b
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-07-29 16:33:47 +0530

    Time: 2023 ms (26.27%), Space: 218.5 MB (62.02%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
@@ -1,71 +1,47 @@
-#define p 10000007
-//#define lli long long int
-
-vector<int> roll;
-
 class Solution {
-    bool match(string s,int len,int size,string& ans)
-    {
-        unordered_map<int,vector<int>> m;   //Key->hashValue...Value->starting index of substring
-        int hash=0;   //curr window hash
-        for(int i=0;i<size;++i)
-            hash = (hash*26 + (s[i]-'a'))%p;
-        
-        m[hash].push_back(0);
-        //Rolling hash (sliding window)
-        for(int j=size;j<len;++j)
-        {
-            hash = ((hash-roll[size-1]*(s[j-size]-'a'))%p + p)%p;
-            hash = (hash*26 + (s[j]-'a'))%p;
-            if(m.find(hash)!=m.end())
-            {
-                for(auto start: m[hash])
-                {
-                    string temp = s.substr(start,size);
-                    string curr = s.substr(j-size+1,size);
-                    if(temp.compare(curr)==0)
-                    {
-                        ans = temp;
-                        return true;
-                    }
-                }
-            }           
-            m[hash].push_back(j-size+1);
-        }
-        return false;
-    }
-
+    using ull = unsigned long long;
 public:
-    string longestDupSubstring(string S) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        int len = S.size();
-        if(len==0)
-            return "";
-        
-        //Store all rolling hash values
-        roll.resize(len);  //Allocating fixed space to array
-        roll[0] = 1;    //Since 26^0 = 1
-        for(int i=1;i<len;++i)
-            roll[i] = (26*roll[i-1])%p;
-        
-        int low=1,high=len,mid;
-        string ans = "",temp;
-        while(low<=high)
-        {
-            temp="";
-            mid = low+(high-low)/2;
-            bool flag = match(S,len,mid,temp);
-            if(flag)
-            {
-                if(temp.size()>ans.size())
-                    ans=temp;
-                low = mid+1;
-            }
-            else
-                high = mid-1;
+    string longestDupSubstring(string s) {
+        int n = s.size();
+        vector<ull> power(n, 1);
+        for (int i=1; i<n; i++) {
+            power[i] = power[i-1] * 27; 
         }
-        return ans;
+        return solve(s, power);
+    }
+    
+    string solve(string& s, vector<ull>& power) {
+        int low = 1, high = s.size();
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if (check(s, mid, power) != -1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        int start = check(s, low-1, power);
+        if (start > 0) return s.substr(start, low-1);
+        return "";
+    }
+    
+    int check(string&s, int len, vector<ull>& power) {
+        if (len <= 0) return -1;
+        unordered_set<ull> st;
+        ull curr_hash = 0;
+        for(int i=0; i<len;i++) {
+            curr_hash = curr_hash + power[len-i-1] * (s[i] - 'a');
+        }
+        st.insert(curr_hash);
+        for(int i=len; i<s.size(); i++) {
+            curr_hash = curr_hash - power[len-1] * (s[i-len] - 'a');
+            curr_hash = curr_hash * 27;
+            curr_hash = curr_hash + (s[i]-'a');
+            if (st.count(curr_hash)) {
+                return i-len+1;
+            }
+            st.insert(curr_hash);
+        }
+        return -1;
     }
 };
